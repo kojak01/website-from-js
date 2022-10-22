107a5bcc0c3c0aaa64357aa5f7a1b6b454e15979
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-10-23 01:14:01 +0200

    add class active to the clicked link and event.preventDefault

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const titleClickHandler = function(event){
+    event.preventDefault();
+    const clickedElement = this;
     console.log('Link was clicked!');
-    console.log(event);
     /* remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -8,7 +9,8 @@ const titleClickHandler = function(event){
     activeLink.classList.remove('active');
     }
     /* add class 'active' to the clicked link */
-  
+    clickedElement.classList.add('active');
+    console.log('clickedElement:', clickedElement);
     /* remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts article.active');
 
