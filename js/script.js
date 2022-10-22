32352b9ca6eaa438b93e1266d1497c16757cf827
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-10-22 23:29:13 +0200

    loop for of!

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 'use strict';
-document.getElementById('test-button').addEventListener('click', function(){
-    const links = document.querySelectorAll('.titles a');
-    console.log('links:', links);
-});
+const links = document.querySelectorAll('.titles a');
+
+for(let link of links){
+  console.log(link);
+}
