253180cfbdd9944a9fd4ee1056b96651e485163a
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-10-23 19:13:44 +0200

    remove contents of titleList

--- js/script.js
+++ js/script.js
@@ -26,10 +26,38 @@ const titleClickHandler = function(event){
     /* add class 'active' to the correct article */
     targetArticle.classList.add('active');
     console.log(clickedElement);
-  }
+  } 
   
   const links = document.querySelectorAll('.titles a');
   
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
+
+const optArticleSelector = '.post', // script settings in variables
+optTitleSelector = '.post-title',
+optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  function clearTitleList(){
+    titleList.innerHTML = '';
+  }
+  clearTitleList();
+  /* for each article */
+
+    /* get the article id */
+
+    /* find the title element */
+
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+
+    /* insert link into titleList */
+
+}
+
+generateTitleLinks();
