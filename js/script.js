7be0d3d5f7dc44a5348aa4ad0548276a6e0c16af
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-10-23 19:50:16 +0200

    create HTML of the link

--- js/script.js
+++ js/script.js
@@ -59,7 +59,8 @@ function generateTitleLinks(){
     /* get the title from the title element */
 
     /* create HTML of the link */
-
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '<span></a></li>';
+    console.log(linkHTML);
     /* insert link into titleList */
   }
 }
