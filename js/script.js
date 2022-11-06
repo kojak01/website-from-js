aeb833685a11835cdb553a16de281cde897247ce
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-11-06 21:33:16 +0100

    add remove active Article funtion

--- js/script.js
+++ js/script.js
@@ -7,7 +7,10 @@ const titleClickHandler = function(e){
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
-
+  const activeArticles = document.querySelectorAll('.posts article.active');
+  for(let activeArticle of activeArticles) {
+    activeArticle.classList.remove('active');
+  }
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attrib
