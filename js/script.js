8dade395d6578b807360832a1e5baccaf801544c
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-11-06 22:01:44 +0100

    add href attribute from the clicked link

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
 const titleClickHandler = function(e){
   event.preventDefault();
   const clickedElement = this;
-  console.log(e);
   /* [DONE] remove class 'active' from all article links */
   const activeLinks = document.querySelectorAll('.titles a.active');
   for(let activeLink of activeLinks) {
     activeLink.classList.remove('active');
   }
   /* [DONE] add class 'active' to the clicked link */
-    console.log('clickedElement: ', clickedElement);
+    //console.log('clickedElement: ', clickedElement);
     clickedElement.classList.add('active');
 
   /* [DONE] remove class 'active' from all articles */
@@ -17,7 +16,8 @@ const titleClickHandler = function(e){
     activeArticle.classList.remove('active');
   }
   /* get 'href' attribute from the clicked link */
-
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
   /* find the correct article using the selector (value of 'href' attrib
 
   /* add class 'active' to the correct article */
