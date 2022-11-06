357cea04ce86283ca8114658b9ee4976dbaeb371
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-11-06 21:25:15 +0100

    add remove active Links function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-const links = document.querySelectorAll('.titles a');
+const titleClickHandler = function(e){
+  /* remove class 'active' from all article links */
+  const activeLinks = document.querySelectorAll('.titles a.active');
+  for(let activeLink of activeLinks) {
+    activeLink.classList.remove('active');
+  }
+  /* add class 'active' to the clicked link */
 
-for(let link of links) {
-  console.log(link);
-}
+  /* remove class 'active' from all articles */
+
+  /* get 'href' attribute from the clicked link */
+
+  /* find the correct article using the selector (value of 'href' attrib
+
+  /* add class 'active' to the correct article */
+
+ }
+ const links = document.querySelectorAll('.titles a');
+ for(let link of links){
+  link.addEventListener('click', titleClickHandler);
+ }
