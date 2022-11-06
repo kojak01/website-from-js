b562e7646f48fe419b81a56916a14737985be327
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-11-06 20:22:55 +0100

    event click

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const Przycisk = document.getElementById('test-button').addEventListener('click', () => {
+  const links = document.querySelectorAll('.titles a');
+  console.log('linki ', links);
+})
