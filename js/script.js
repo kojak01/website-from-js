a207f46a9ae75c71a83368fc9006682974c3fba9
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-11-07 19:54:39 +0100

    fix html

--- js/script.js
+++ js/script.js
@@ -15,16 +15,43 @@ const titleClickHandler = function(e){
   for(let activeArticle of activeArticles) {
     activeArticle.classList.remove('active');
   }
-  /* get 'href' attribute from the clicked link */
+  /* [DONE]  get 'href' attribute from the clicked link */
     const articleSelector = clickedElement.getAttribute('href');
     //console.log(articleSelector);
-  /* find the correct article using the selector (value of 'href' attribute) */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
     const targetArticle = document.querySelector(articleSelector)
     console.log(targetArticle);
-  /* add class 'active' to the correct article */
+  /* [DONE] add class 'active' to the correct article */
     targetArticle.classList.add('active');
  }
- const links = document.querySelectorAll('.titles a');
- for(let link of links){
+ 
+
+ const optArticleSelector = '.post',
+ optTitleSelector = '.post-title',
+ optTitleListSelector = '.titles';
+
+ function generateTitleLinks(){
+  /* [DONE] remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+  /* [DONE] for each article */
+  const articles = document.querySelectorAll(optArticleSelector);
+  let html = '';
+  for(let article of articles) {
+    /* [DONE] get the article id */
+  const articleId = article.getAttribute('id');
+  /* [DONE] find the title element */
+  const articleTitle = article.querySelector(optTitleSelector).innerHTML;  
+  /* [DONE] create HTML of the link */
+  const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+  /* insert link into titleList */
+  html = html + linkHTML;
+  //console.log(html);
+  };
+  titleList.innerHTML = html;
+  const links = document.querySelectorAll('.titles a');
+    for(let link of links){
   link.addEventListener('click', titleClickHandler);
- }
+ };
+};
+generateTitleLinks();
